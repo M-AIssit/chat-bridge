42108b4ea4af934c3f136f1578ed99badb09cb0a
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2024-08-08 16:09:20 +0200

    solve

--- modules/api/users.py
+++ modules/api/users.py
@@ -17,48 +17,44 @@ BUSINESS_OWNER_PHONE_NUMBER = os.getenv("BUSINESS_OWNER_PHONE_NUMBER")
 def create_user():
     global current_to_number
 
-    message_sid = request.form.get('MessageSid')
-    sms_sid = request.form.get('SmsSid')
-    sms_message_sid = request.form.get('SmsMessageSid')
-    account_sid = request.form.get('AccountSid')
-    messaging_service_sid = request.form.get('MessagingServiceSid')
+    # Extracción de datos del formulario de solicitud
     from_number = request.form.get('From')
-    to_number = request.form.get('To')
     body = request.form.get('Body')
-    num_media = request.form.get('NumMedia')
-    num_segments = request.form.get('NumSegments')
 
-    print("Message SID:", message_sid)
-    print("SMS SID:", sms_sid)
-    print("SMS Message SID:", sms_message_sid)
-    print("Account SID:", account_sid)
-    print("Messaging Service SID:", messaging_service_sid)
+    # Registro de datos de entrada
     print("From Number:", from_number)
-    print("To Number:", to_number)
     print("Body:", body)
-    print("Number of Media:", num_media)
-    print("Number of Segments:", num_segments)
 
-    # Verificar si el número de teléfono ya está en el CSV
+    # Comprobación de la información del teléfono en el CSV
     phone_info = check_phone_number(from_number)
-    if phone_info:
-        detected_language = phone_info['language']
+    if not phone_info:
+        detected_language_json = detect_language(body)
+        try:
+            detected_lang_data = json.loads(detected_language_json)
+            if 'ISO 639-1' in detected_lang_data:
+                add_phone_number(from_number, detected_lang_data['language'], detected_lang_data['ISO 639-1'])
+                detected_language = detected_lang_data['language']
+                iso_code = detected_lang_data['ISO 639-1']
+            else:
+                print("No ISO code provided in the language detection response.")
+                return "Error handling request"
+        except json.JSONDecodeError:
+            print("Failed to decode the language detection response.")
+            return "Error handling request"
     else:
-        detected_language = detect_language(body)
-        detected_lang_data = json.loads(detected_language)  # Asumiendo que detect_language devuelve JSON con datos de idioma
-        add_phone_number(from_number, detected_lang_data['language'], detected_lang_data['ISO 639-1'])
+        detected_language = phone_info['language']
+        iso_code = phone_info['ISO 639-1']
 
-    # Traducir el cuerpo del mensaje
-    translated_text = translate_text(body, detected_language, phone_info["ISO 639-1"])
+    # Traducción del mensaje
+    translated_text = translate_text(body, detected_language, iso_code)
     print("Detected Language:", detected_language)
     print("Translated Text:", translated_text)
 
-    # Convertir el texto traducido a formato JSON
+    # Conversión del texto traducido a JSON y preparación para el envío
     translated_text_json = json.loads(translated_text)
     translated_text_json['from_number'] = from_number
-    print("Translated Text JSON:", translated_text_json)
 
-    # Lógica de envío de mensajes de WhatsApp
+    # Envío del mensaje traducido según la lógica de negocio
     if from_number != BUSINESS_OWNER_PHONE_NUMBER:
         send_whatsapp_message(translated_text_json, BUSINESS_OWNER_PHONE_NUMBER)
     else:
